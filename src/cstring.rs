b594c3c1af9c6d1e37b52f73d99b47b53b381ffe
CraftSpider <runetynan@gmail.com>
2024-05-27 14:00:50 -0700

    Add CString documentation

--- src/cstring.rs
+++ src/cstring.rs
@@ -2,10 +2,10 @@
 
 use alloc::vec::Vec;
 use core::borrow::{Borrow, BorrowMut};
-use core::marker::PhantomData;
-use core::ops::{Deref, DerefMut};
 use core::fmt;
 use core::hash::{Hash, Hasher};
+use core::marker::PhantomData;
+use core::ops::{Deref, DerefMut};
 
 use crate::cstr::CStr;
 use crate::encoding::{Encoding, NullTerminable, ValidateError};
@@ -14,13 +14,20 @@ pub use crate::err::RecodeError;
 use crate::str::Str;
 use crate::string::String;
 
+/// The cause of an error while creating a [`CString`]
 #[derive(Debug, PartialEq)]
 #[non_exhaustive]
 pub enum CStringErrorCause {
+    /// The input wasn't valid for the desired encoding
     Invalid(ValidateError),
-    HasNull { idx: usize },
+    /// The input contains a null byte not in the final position
+    HasNull {
+        /// The position of the null byte in the input
+        idx: usize,
+    },
 }
 
+/// An error encountered while creating a new [`CString`] from a container of bytes
 #[derive(Debug, PartialEq)]
 pub struct CStringError {
     bytes: Vec<u8>,
@@ -28,15 +35,18 @@ pub struct CStringError {
 }
 
 impl CStringError {
+    /// Get the cause of this error
     pub fn cause(&self) -> &CStringErrorCause {
         &self.cause
     }
 
+    /// Consume this error, returning the input bytes which generated the error in the first place.
     pub fn into_vec(self) -> Vec<u8> {
         self.bytes
     }
 }
 
+/// An error encountered while converting a [`String`] into a [`CString`]
 #[derive(Debug, PartialEq)]
 pub struct NulError {
     bytes: Vec<u8>,
@@ -44,28 +54,55 @@ pub struct NulError {
 }
 
 impl NulError {
+    /// Returns the position of the null byte in the input that caused [`CString::try_from`] to
+    /// fail.
     pub fn nul_position(&self) -> usize {
         self.nul_pos
     }
 
+    /// Consume this error, returning the input bytes which generated the error in the first place.
     pub fn into_vec(self) -> Vec<u8> {
         self.bytes
     }
 }
 
+/// A type representing an owned, generically-encoded C-string. This means the string contains a
+/// single trailing null byte, with no other null bytes internally.
+///
+/// This type is to [`CStr`] as [`String`] is to [`Str`] - it represents the owned form of C string,
+/// while [`CStr`] represents the borrowed form.
 pub struct CString<E>(PhantomData<E>, Vec<u8>);
 
 impl<E: Encoding + NullTerminable> CString<E> {
+    /// Create a C string from a byte vector, without checking for interior null
+    /// bytes or valid encoding. This method is similar to [`CString::new`],
+    /// but without validity checking.
+    ///
+    /// The trailing null byte will be appended by this method.
+    ///
+    /// # Safety
+    ///
+    /// The provided vector must contain no null bytes and be valid for the
+    /// current encoding.
     pub unsafe fn from_vec_unchecked(mut bytes: Vec<u8>) -> CString<E> {
         bytes.push(0);
         CString(PhantomData, bytes)
     }
 
+    /// Create a new C string from a container of bytes. The provided data should contain no null
+    /// bytes.
+    ///
+    /// This function will consume and validate the provided data, checking that it contains no null
+    /// bytes and is valid for the current encoding. If those checks pass, a single null byte is
+    /// appended to the end.
+    ///
+    /// If you have a [`String<E>`], you should prefer the [`CString::try_from`] implementation.
+    /// It is capable of skipping the encoding check and only validating the lack of null bytes.
     pub fn new<T>(bytes: T) -> Result<CString<E>, CStringError>
     where
         T: Into<Vec<u8>>,
     {
-        let mut bytes = bytes.into();
+        let bytes = bytes.into();
         let nul_pos = bytes.iter().position(|b| *b == 0);
         if let Some(idx) = nul_pos {
             return Err(CStringError {
@@ -83,15 +120,19 @@ impl<E: Encoding + NullTerminable> CString<E> {
         Ok(unsafe { Self::from_vec_unchecked(bytes) })
     }
 
+    /// Convert this `CString` into a [`String`] by removing the trailing null. Unlike the
+    /// equivalent `std` method, this is infallible because our `CString` is encoding-specific.
     pub fn into_string(self) -> String<E> {
         unsafe { String::from_bytes_unchecked(self.into_bytes()) }
     }
 
+    /// Convert this `CString` into bytes, minus the trailing null byte
     pub fn into_bytes(mut self) -> Vec<u8> {
         self.1.pop();
         self.1
     }
 
+    /// Convert this `CString` into bytes, including the trailing null byte
     pub fn into_bytes_with_nul(self) -> Vec<u8> {
         self.1
     }
@@ -175,8 +216,20 @@ impl<E: NullTerminable> TryFrom<String<E>> for CString<E> {
         // so only need to check for an inner null byte.
         let bytes = value.into_bytes();
         if let Some(nul_pos) = bytes.iter().position(|b| *b == 0) {
-            return Err(NulError { bytes, nul_pos })
+            return Err(NulError { bytes, nul_pos });
         }
+        Ok(unsafe { CString::from_vec_unchecked(bytes) })
+    }
+}
+
+impl<E: NullTerminable> TryFrom<std::ffi::CString> for CString<E> {
+    type Error = ValidateError;
+
+    fn try_from(value: std::ffi::CString) -> Result<Self, Self::Error> {
+        let bytes = value.into_bytes();
+        E::validate(&bytes)?;
+        // SAFETY: An std CString is guaranteed to contain no internal null bytes
+        //         Bytes have been validated
         Ok(unsafe { CString::from_vec_unchecked(bytes) })
     }
 }
